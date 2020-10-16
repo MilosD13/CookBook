7d5918d751b488132cc138a586b6cd9319b03636
Milos Dokic <milosdokic13@gmail.com>
2020-10-16 22:50:54 +1100

    creating nav bar in entries section

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 
 const entryForm = document.getElementById("entryForm");
 const entriesSection = document.getElementById("entries");
-const entryTextbox = document.getElementsByClassName("entry-textbox");
+const entryTextbox = document.querySelector(".entry-textbox");
+const entriesNav = document.querySelector('.entries-nav');
 
+let count = 1;
 function addEntryToDom(event) {
     event.preventDefault();
     const entryDiv = document.createElement("div");
     entryDiv.className = "single-entry"
-    entryDiv.innerText = entryTextbox[0].value;
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = "none";
     entriesSection.appendChild(entryDiv);
-    entryTextbox[0].value = "";
+    entryTextbox.value = "";
 
+    const displayEntryButton = document.createElement("button");
+    displayEntryButton.className = "display-entry-button";
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
+
+    displayEntryButton.addEventListener("click", function () {
+        entryDiv.style.display = "block"
+    })
+    count++
 }
 
 entryForm.addEventListener("submit", addEntryToDom)
